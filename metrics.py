8c2856a80be8d35863d9d6b02724c78a4906f9a0
Andrew Ying <andrewying@DN0a2265da.SUNet>
2019-12-09 23:26:50 -0800

    add azimuthal

--- metrics.py
+++ metrics.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm, PowerNorm, Normalize
 import tensorflow as tf
 from models import dcgan, utils
+from scipy import fftpack
 
 checkpoint_dir = 'cosmoGAN_pretrained_weights'
 validation_set_location = 'data/cosmogan_maps_256_8k_1.npy' # TODO (team): replace with validation set link
@@ -37,9 +38,112 @@ with tf.Graph().as_default() as g:
         plt.legend(loc='upper right')
         plt.savefig('histo', format='png')
 
-# def pixel_intensity(validation_set, generator, noise_vector_length, inverse_transform, channel_axis):
-#     """First order metric: histogram of pixel intensities
-#     Used to compare validation set with the generator set."""
-#
-#     num = len(validation_set)
-#     samples = generator.predict(np.random.normal(size=(num, 1, noise_vector_length)))
+
+def azimuthalAverage(image, center=None):
+    """
+    Calculate the azimuthally averaged radial profile.
+    image - The 2D image
+    center - The [x,y] pixel coordinates used as the center. The default is
+             None, which then uses the center of the image (including
+             fracitonal pixels).
+
+    """
+    # Calculate the indices from the image
+    y, x = np.indices(image.shape)
+
+    if not center:
+        center = np.array([(x.max() - x.min()) / 2.0, (x.max() - x.min()) / 2.0])
+
+    r = np.hypot(x - center[0], y - center[1])
+
+    # Get sorted radii
+    ind = np.argsort(r.flat)
+    r_sorted = r.flat[ind]
+    i_sorted = image.flat[ind]
+
+    # Get the integer part of the radii (bin size = 1)
+    r_int = r_sorted.astype(int)
+
+    # Find all pixels that fall within each radial bin.
+    deltar = r_int[1:] - r_int[:-1]  # Assumes all radii represented
+    rind = np.where(deltar)[0]  # location of changed radius
+    nr = rind[1:] - rind[:-1]  # number of radius bin
+
+    # Cumulative sum to figure out sums for each radius bin
+    csim = np.cumsum(i_sorted, dtype=float)
+    tbin = csim[rind[1:]] - csim[rind[:-1]]
+
+    radial_prof = tbin / nr
+
+    return radial_prof
+
+
+def power_spectrum(image):
+    """Computes azimuthal average of 2D power spectrum of a np array image"""
+    GLOBAL_MEAN = 0.9998563  # this should be the mean pixel value of the training+validation datasets
+    F1 = fftpack.fft2((image - GLOBAL_MEAN) / GLOBAL_MEAN)
+    F2 = fftpack.fftshift(F1)
+    pspec2d = np.abs(F2) ** 2
+    P_k = azimuthalAverage(pspec2d)
+    k = np.arange(len(P_k))
+    return k, P_k
+
+
+def batch_Pk(arr):
+    """Computes power spectrum for a batch of images"""
+    Pk_arr = []
+    for idx in range(arr.shape[0]):
+        k, P_k = power_spectrum(np.squeeze(arr[idx]))
+        Pk_arr.append(P_k)
+    return k, np.array(Pk_arr)
+
+
+def pspect(val_imgs, generator, invtransform, noise_vect_len, channel_axis, fname=None, Xterm=True, multichannel=False):
+    """plots mean and std deviation of power spectrum over validation set + generated samples"""
+    num = val_imgs.shape[0]
+    gen_imgs = generator.predict(np.random.normal(size=(num, 1, noise_vect_len)))
+    if multichannel:
+        gen_imgs = np.take(gen_imgs, 0, axis=channel_axis)  # take the scaled channel
+    else:
+        gen_imgs = np.squeeze(gen_imgs)
+    gen_imgs = invtransform(gen_imgs)
+    k, Pk_val = batch_Pk(val_imgs)
+    k, Pk_gen = batch_Pk(gen_imgs)
+
+    val_mean = np.mean(Pk_val, axis=0)
+    gen_mean = np.mean(Pk_gen, axis=0)
+    val_std = np.std(Pk_val, axis=0)
+    gen_std = np.std(Pk_gen, axis=0)
+
+    plt.figure()
+    plt.fill_between(k, gen_mean - gen_std, gen_mean + gen_std, color='red', alpha=0.4)
+    plt.plot(k, gen_mean, 'r--')
+    plt.plot(k, val_mean, 'k:')
+    plt.plot(k, val_mean + val_std, 'k-')
+    plt.plot(k, val_mean - val_std, 'k-')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.ylabel(r'$P(k)$')
+    plt.xlabel(r'$k$')
+    plt.title('Power Spectrum')
+    if Xterm:
+        plt.draw()
+    else:
+        plt.savefig(fname, format='png')
+        plt.close()
+    return np.sum(np.divide(np.power(gen_mean - val_mean, 2.0), val_mean))
+
+def calculate_correlation_matrix(corr_matrix):
+    newmat = np.zeros(corr_matrix.shape)
+    for i in range(corr_matrix.shape[0]):
+        for j in range(corr_matrix.shape[1]):
+            newmat[i,j] = corr_matrix[i,j] / np.sqrt(corr_matrix[i,i] * corr_matrix[j,j])
+    return newmat
+
+_, Pk_val = batch_Pk(validation_set)
+# _, Pk_gen = batch_Pk(generated_images)
+# correlation matrix for validation
+plt.matshow(calculate_correlation_matrix(Pk_val), cmap='RdBu', vmin=-1., vmax=1.)
+# plt.matshow(calculate_correlation_matrix(Pk_gen), cmap='RdBu', vmin=-1., vmax=1.)
+plt.colorbar()
+plt.show()
