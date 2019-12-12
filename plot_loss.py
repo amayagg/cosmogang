ed675f2f758610eed39858e0580f71a83869b86a
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-12 03:40:20 +0000

    plt loss

--- plot_loss.py
+++ plot_loss.py
@@ -9,7 +9,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-infile = "output/geogan.log"
+infile = "output/geogan_master.log"
 
 with open(infile) as f:
     f = f.readlines()
@@ -33,19 +33,37 @@ d_losses = np.array(d_losses)
 #g_losses = g_losses[:400]
 #d_losses = d_losses[:400]
 
-common_factor = 5
-g_losses = np.mean(g_losses.reshape(-1, common_factor), axis=1) # shrink by common_factor
-d_losses = np.mean(d_losses.reshape(-1, common_factor), axis=1)
+#common_factor = 5
+#g_losses = np.mean(g_losses.reshape(-1, common_factor), axis=1) # shrink by common_factor
+#d_losses = np.mean(d_losses.reshape(-1, common_factor), axis=1)
 
 #g_losses = g_losses[:100]
 #d_losses = d_losses[:100]
 
-print(g_losses)
+#print(g_losses)
+
+def moving_average(arr, window):
+    """
+    http://stackoverflow.com/questions/14313510/how-to-calculate-moving-average-using-numpy
+    """
+    ret = np.cumsum(arr)
+    ret[window:] = ret[window:] - ret[:-window]
+    return ret[window - 1:] / window
+
+
+#arr = np.arange(20)
+g_losses = moving_average(g_losses, window=100)
+d_losses = moving_average(d_losses, window=100)
 
 plt.plot(g_losses, label = "G Loss")
-plt.plot(d_losses, label = "D Loss")
-
 plt.legend()
-plt.xlabel("Epochs")
+plt.xlabel("Iterations")
 plt.ylabel("Loss")
-plt.savefig("losses/geogan_105.png")
+plt.savefig("losses/geogan_gen_loss.png")
+
+plt.figure()
+plt.plot(d_losses, label = "D Loss")
+plt.legend()
+plt.xlabel("Iterations")
+plt.ylabel("Loss")
+plt.savefig("losses/geogan_dic_loss.png")
